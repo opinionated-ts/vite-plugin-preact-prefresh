1e849f11d996d1d2d003108ee8ee006148ebb45b
DanhezCode <danhezcode@gmail.com>
2026-08-19 17:09:45 -0400

    feat: releases are now available

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// oxlint-disable-next-line import/no-default-export
+export { default, prefresh, type PrefreshOptions } from "@/prefresh";
---
+++ tsdown.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "tsdown";
+
+// oxlint-disable-next-line import/no-default-export
+export default defineConfig({
+  dts: true,
+  exports: true,
+});
